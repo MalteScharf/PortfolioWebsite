496a52d7d847e4f2639769f8c3ff0c52fec1704d
Malte Scharf <malte.scharf@hotmail.de>
2025-07-07 15:09:03 +0200

    include Vercel Analytics in JS Script #5

--- js/vercelTracking.js
+++ js/vercelTracking.js
@@ -3,5 +3,6 @@ import { track } from 'https://esm.sh/@vercel/analytics';
 // Download CV
 window.trackCV = () => {
   track('download_cv_clicked');
+  console.log("Tracked: download_cv_clicked");
   window.open('./img/CV_webversion.pdf', '_blank');
 }
